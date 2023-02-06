6709bd3ab2070efcd41e9e6ddb3f91782452a24f
Jungsik Won <wl808@naver.com>
2023-02-06 17:51:02 +0900

    30p 연습문제 풀이

--- src/javaAlgorithm/basic/Page30.java
+++ src/javaAlgorithm/basic/Page30.java
@@ -1,11 +1,24 @@
 package javaAlgorithm.basic;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Page30 {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         int n = 7;
+        int a, b;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
 
         method1(n);
         method2(n);
+        method3(a, b);
     }
 
     private static void method1(int n){
@@ -18,6 +31,14 @@ public class Page30 {
 
     private static void method2(int n){
         int sum = n * (n + 1) / 2;
+        System.out.println(sum);
+    }
+
+    private static void method3(int a, int b){
+        int sum = 0;
+        for(int i = a; i <= b; i++){
+            sum += i;
+        }
         System.out.println(sum);
     }
 }
