5d9b4892676f3715af46a7f2ef2fb9812485e6c3
Jungsik Won <wl808@naver.com>
2023-02-13 18:25:32 +0900

    15727번 풀이

--- src/baekjoon/bronze/No15727.java
+++ src/baekjoon/bronze/No15727.java
@@ -1,9 +1,18 @@
 package baekjoon.bronze;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class No15727 {
     public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int L = Integer.parseInt(br.readLine());
+        int result;
 
+        if(L % 5 != 0)  result = L / 5 + 1;
+        else            result = L / 5;
+
+        System.out.print(result);
     }
 }
